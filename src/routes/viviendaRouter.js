519f8a6369237e43a2afce76ee2363e03ce94346
Harrison <harrison.pinto@d4d.com.co>
2022-04-05 02:38:43 -0500

    Implement delete, update and read operations
    A bug might pop up in the future, refering to the way we are using the
    `findOne` method.
    
    Co-authored-by: Sebastian Garnica <SGman98@users.noreply.github.com>

--- src/routes/viviendaRouter.js
+++ src/routes/viviendaRouter.js
@@ -51,4 +51,83 @@ router
 		return res.json({ ...vivienda, owners: promises });
 	});
 
+router
+	.route("/:id")
+	.patch(async (req, res) => {
+		const { id } = req.params;
+		const { direccion, capacidad, niveles, id_municipio, owners } = req.body;
+		// update the vivienda
+		const update = await db.models.vivienda.update(
+			{ direccion, capacidad, niveles, id_municipio },
+			{ where: { id } }
+		);
+
+		// associate the vivienda with the owners
+		let vivienda_id = id;
+		let promises = [];
+
+		// delete the old owners
+		await db.models.persona_has_vivienda.destroy({
+			where: { id_vivienda: vivienda_id },
+		});
+
+		// add the new owners
+		owners.forEach((owner) => {
+			promises.push(
+				db.models.persona_has_vivienda
+					.create({
+						id_persona: owner.id,
+						id_vivienda: vivienda_id,
+					})
+					.catch((error) => {
+						console.log({ error });
+						return error;
+					})
+			);
+		});
+
+		await Promise.all(promises);
+
+		// send the updated vivienda as JSON response
+		return res.json({ ...update, owners: promises });
+	})
+	.delete(async (req, res) => {
+		const { id } = req.params;
+
+		// delete the vivienda associations
+		await db.models.persona_has_vivienda.destroy({
+			where: { id_vivienda: id },
+		});
+
+		// delete the vivienda
+		const deleteVivienda = await db.models.vivienda.destroy({
+			where: { id },
+		});
+
+		// send the deleted vivienda as JSON response
+		return res.json(deleteVivienda);
+	})
+	.get(async (req, res) => {
+		const { id } = req.params;
+
+		// FIXME: This might not work
+		// get the vivienda
+		const vivienda = await db.models.vivienda.findOne({
+			where: { id },
+			include: [
+				{
+					model: db.models.persona_has_vivienda,
+					include: [
+						{
+							model: db.models.persona,
+						},
+					],
+				},
+			],
+		});
+
+		// send the vivienda as JSON response
+		return res.json(vivienda);
+	});
+
 module.exports = router;
